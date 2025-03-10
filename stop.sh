9455776e223c05e809823b8a4b53ea32345975c1
Tamas Biro <tamas.biro@liferay.com>
2025-03-10 10:18:50 +0100

    INFOSEC-2977 Mysql container goes down when the last running DXP is shutting down

--- stop.sh
+++ stop.sh
@@ -9,7 +9,6 @@ echo "Docker container '${CONTAINER_ID}' is stopped."
 docker rm -f $CONTAINER_ID
 echo "Docker container '${CONTAINER_ID}' is removed."
 
-
 # MySQL container name
 MYSQL_CONTAINER="mysql"
 
@@ -24,3 +23,11 @@ SQL_COMMAND="DROP DATABASE $ID_NAME;"
 docker exec -i $MYSQL_CONTAINER mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "$SQL_COMMAND"
 
 echo "MySQL database '${ID_NAME}' is removed."
+
+# Stop Mysql container if there is no more container that's image name starts with "liferay"
+IS_ANY_LIFERAY_CONTAINER=$(docker ps --format "{{.Image}}" | grep "^liferay")
+
+if [[ -z "$IS_ANY_LIFERAY_CONTAINER" ]]; then
+    echo "No more running liferay container, stop Mysql container..."
+    docker stop mysql
+fi
